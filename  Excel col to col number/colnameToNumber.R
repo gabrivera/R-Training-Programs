657773822db9b4687d53a676fb160d6f248e50f7
Gabriel Rivera Barreto <gabrielpaipa@gmail.com>
2021-02-06 21:06:09 -0600

    Initial commit

---
+++  Excel col to col number/colnameToNumber.R
@@ -0,0 +1,34 @@
+colnameToNumber <- function(colname) {
+    
+    # Make sure colname is a lower-case!
+    colname = tolower(colname)
+    
+    # Create all the letters
+    allTheLetters = letters
+    for (j in letters){
+        for (i in letters) {
+            allTheLetters = c( allTheLetters, paste(j, i, sep = "") )
+        }
+    }
+    
+    # Create the vector that will be populated with the results
+    colnumber = numeric()
+    
+    # Iterate over the colnames and see whether there are matches
+    for ( i in colname ){
+        tempColNumber = numeric()
+        tempColNumber = which(allTheLetters == i)
+        if( length(tempColNumber) == 0 ) { stop("no match for colname") }
+        else colnumber = c(colnumber, tempColNumber)
+    }
+    
+    return(colnumber)
+    
+    # convert an Excel column name
+    # to the corresponding column number;
+    # colname should be a lower case letter in the
+    # form of a character string, for example "a" or "r" 
+    # "between" a and z (including a and z)
+    #   coding lines
+    #return(colnumber)
+}
